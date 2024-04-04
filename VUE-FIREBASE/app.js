455cbf2162abcf22854dc9c565428607b420102e
Kari Smith <kariasmith@hotmail.com>
2024-04-03 20:30:01 -0600

    Update app.js

--- VUE-FIREBASE/app.js
+++ VUE-FIREBASE/app.js
@@ -3,7 +3,8 @@ const app = Vue.createApp({
         return{
             commedian: 'Ralphie May',
             special: 'Too Big To Ignore',
-            joke: 'Stoned Like a Gravel Road'
+            joke: 'Stoned Like a Gravel Road',
+            Likes: 0
         }
     }
 })
