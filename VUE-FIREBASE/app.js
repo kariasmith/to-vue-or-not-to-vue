ff53fd729206160a144d7254f5e5f933be129dcd
Kari Smith <kariasmith@hotmail.com>
2024-04-17 09:49:49 -0600

    Update app.js

--- VUE-FIREBASE/app.js
+++ VUE-FIREBASE/app.js
@@ -1,28 +1,28 @@
 const app = Vue.createApp({
+    // data, functions, templates
     data() {
-        return{
-            showJoke: true,
-            comedys: [
-                { comedian: 'Ralphie May', special: 'Too Big To Ignore', joke: 'Stoned Like a Gravel Road'},
-                { comedian: 'Ralphie May', special: 'Too Big To Ignore', joke: 'Stoned Like a Gravel Road'},
-                { comedian: 'Ralphie May', special: 'Too Big To Ignore', joke: 'Stoned Like a Gravel Road'},
+        return {
+            showBooks: true,
+            books: [
+                { title: 'The Hobbit', author: 'J. R. R. Tolkien', year: 1937, img: 'assets/the-hobbit.jpg', isFav: true },
+                { title: 'The Fellowship of the Ring', author: 'J. R. R. Tolkien', year: 1954, img: 'assets/the-fellowship.jpg', isFav: true },
+                { title: 'The Two Towers', author: 'J. R. R. Tolkien', year: 1954, img: 'assets/the-two-towers.jpg', isFav: false },
+                { title: 'The Return of the King', author: 'J. R. R. Tolkien', year: 1955, img: 'assets/the-return-ofthe-king.jpg', isFav: false },
             ],
-            Likes: 0,
+            url: "https://www.tolkiensociety.org/author/books-by-tolkien/",
+            likes: 0,
             x: 0,
             y: 0
         }
     },
     methods: {
-        toggleShowJokes() {
-            this.showJoke = !this.showJoke
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks
         },
-        eventAction(k, data) {
-            console.log(k)
-            if (data) {
-                console.log(data)
-            }
+        handleEvent(e) {
+            console.log(e)
         },
-        handleMousemove(e){
+        handleMousemove(e) {
             this.x = e.offsetX
             this.y = e.offsetY
         }
@@ -30,3 +30,4 @@ const app = Vue.createApp({
 })
 
 app.mount('#app')
+
